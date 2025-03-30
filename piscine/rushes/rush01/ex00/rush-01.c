00ee4f5fc5c8d28a5a5c8c8a2c1432766a32983e
Joshho <josh_@Razerr.>
2025-03-30 15:32:19 +0800

    added visibility check function

--- piscine/rushes/rush01/ex00/rush-01.c
+++ piscine/rushes/rush01/ex00/rush-01.c
@@ -6,7 +6,7 @@
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 15:28:34 by joho              #+#    #+#             */
-/*   Updated: 2025/03/29 22:06:01 by joho             ###   ########.fr       */
+/*   Updated: 2025/03/30 15:31:42 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,91 @@ int	numcheck(char *str, int *colups, int *coldowns, int *rowlefts, int *rowright
 	return (1);
 }
 
-int	visible(int *val, int grid[][], int *colups, int *coldowns, int *rowlefts, int *rowrights)
+int	visible(int *val, int grid[size][size], int size, int *colups, int *coldowns, int *rowlefts, int *rowrights)
 {
 	int	i;
 	i = 0;
 
 	while(val[i])
 	{
-		
+		int row;
+		int col;
+		int	vis_count;
+		int	max_height;
+		int	k;
+
+		row = i / size;
+		col = i % size;
+		grid[row][col] = val[i];
+
+		if (colups[col] != 0)
+		{
+			vis_count = 1;
+			max_height = grid[0][col];
+			k = 1;
+			while(k < size)
+			{
+				if (grid[k][col] > max_height)
+				{
+					vis_count++;
+					max_height = grid[k][col];
+				}
+			}
+			if (vis_count != colups[col])
+				return (0);
+		}
+		if (coldowns[col] != 0)
+		{
+			vis_count = 1;
+			max_height = grid[size - 1][col];
+			k = size - 2;
+			while (k >= 0)
+			{
+				if (grid[k][col] > max_height)
+				{
+					vis_count++;
+					max_height = grid[k][col];
+				}
+				k--;
+			}
+			if (vis_count != coldowns[col])
+				return (0);
+		}
+		if (rowlefts[row] != 0)
+		{
+			vis_count = 1;
+			max_height = grid[row][0];
+			k = 1;
+			while (k < size)
+			{
+				if (grid[row][k] > max_height)
+				{
+					vis_count++;
+					max_height = grid[row][k];
+				}
+				k++;
+			}
+			if (vis_count != rowlefts[row])
+				return (0);
+		}
+		if (rowrights[row] != 0)
+		{
+			vis_count = 1;
+			max_height = grid[row][size - 1];
+			k = size - 2;
+			while (k > 0)
+			{
+				if (grid[row][k] > max_height)
+				{
+					vis_count++;
+					max_height = grid[row][k];
+				}
+				k--;
+			}
+			if (vis_count != rowrights[row])
+				return (0);
+		}
+		i++;
 	}	
 	return (1);
 }
