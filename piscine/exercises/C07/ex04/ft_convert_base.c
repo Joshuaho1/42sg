e663a2358c0c0ac569ff26f122a9ff8e4ab04501
Josh <josh_ho@rocketmail.com>
2025-04-10 00:54:34 +0800

    updated ex04 and ex05

--- piscine/exercises/C07/ex04/ft_convert_base.c
+++ piscine/exercises/C07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:43:51 by joho              #+#    #+#             */
-/*   Updated: 2025/04/09 21:16:03 by joho             ###   ########.fr       */
+/*   Updated: 2025/04/10 00:01:04 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,25 @@
 int	valid_base(char *base);
 int	base_index(char c, char *base);
 int	ft_atoi_base(char *str, char *base);
+
+void	create_str(char *result, int length, char *base, long num)
+{
+	int		base_len;
+
+	base_len = valid_base(base);
+	if (num < 0)
+	{
+		num = -num;
+		result[0] = '-';
+	}
+	result[length] = '\0';
+	while (num >= base_len)
+	{
+		result[--length] = base[num % base_len];
+		num /= base_len;
+	}
+	result[--length] = base[num];
+}
 
 char	*ft_itoa_base(int nbr, char *base)
 {
@@ -33,16 +52,7 @@ char	*ft_itoa_base(int nbr, char *base)
 	result = (char *)malloc(sizeof(char) * (length + 1));
 	if (!result)
 		return (NULL);
-	num = nbr;
-	if (num < 0 && (num = -num))
-		result[0] = '-';
-	result[length] = '\0';
-	while (num >= base_len)
-	{
-		result[--length] = base[num % base_len];
-		num /= base_len;
-	}
-	result[--length] = base[num];
+	create_str(result, length, base, nbr);
 	return (result);
 }
 
