de1db238f15683b635d31f72629ecf48ef2bedfb
Josh <josh_ho@rocketmail.com>
2025-04-09 23:20:48 +0800

    revised ft_convert_base

--- piscine/exercises/C07/ex04/ft_convert_base.c
+++ piscine/exercises/C07/ex04/ft_convert_base.c
@@ -6,11 +6,15 @@
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:43:51 by joho              #+#    #+#             */
-/*   Updated: 2025/04/09 17:26:21 by joho             ###   ########.fr       */
+/*   Updated: 2025/04/09 21:16:03 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+int	valid_base(char *base);
+int	base_index(char c, char *base);
+int	ft_atoi_base(char *str, char *base);
 
 char	*ft_itoa_base(int nbr, char *base)
 {
@@ -18,35 +22,27 @@ char	*ft_itoa_base(int nbr, char *base)
 	int		base_len;
 	int		length;
 	long	num;
-	int		i;
 
 	base_len = valid_base(base);
 	length = 1;
 	num = nbr;
-	if (num < 0)
-	{
-		length++;
+	if (num < 0 && ++length)
 		num = -num;
-	}
 	while (num >= base_len && ++length)
 		num /= base_len;
 	result = (char *)malloc(sizeof(char) * (length + 1));
 	if (!result)
 		return (NULL);
 	num = nbr;
-	if (num < 0)
-	{
+	if (num < 0 && (num = -num))
 		result[0] = '-';
-		num = -num;
-	}
-	i = length - 1;
+	result[length] = '\0';
 	while (num >= base_len)
 	{
-		result[i--] = base[num % base_len];
+		result[--length] = base[num % base_len];
 		num /= base_len;
 	}
-	result[i] = base[num];
-	result[length] = '\0';
+	result[--length] = base[num];
 	return (result);
 }
 
@@ -61,3 +57,21 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 	result = ft_itoa_base(dec, base_to);
 	return (result);
 }
+
+/*
+#include <stdio.h>
+
+int main()
+{
+	printf("%s\n", ft_convert_base("     ---123", "01234", "01"));
+	printf("%s\n", ft_convert_base("2147483647", "0123456789", 
+	"0123456789abcdef"));
+	printf("%s\n", ft_convert_base("-2147483648", "0123456789", "012345"));
+	printf("%s\n", ft_convert_base("---7fffffff", "0123456789abcdef", "01"));
+	printf("%s\n", ft_convert_base("---+--0001023a", "0123456789", 
+	"0123456789"));
+	printf("%s\n", ft_convert_base("-0", "0123456789", "abcdef"));
+	printf("%s\n", ft_convert_base("   \t\t\t\n05841fff4", 
+	"0123456789abcdef", "0123"));
+}
+*/
