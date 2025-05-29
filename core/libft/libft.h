81c38f9975e3aec0353e52ef58459854d454639e
Josh <josh_ho@rocketmail.com>
2025-05-29 17:12:06 +0800

    initial commit

---
+++ core/libft/libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 14:24:16 by joho              #+#    #+#             */
+/*   Updated: 2025/05/29 17:02:25 by joho             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT
+# define LIBFT
+
+#include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+void	ft_memset(void *s, int c, size_t n);
+
+
+#endif
