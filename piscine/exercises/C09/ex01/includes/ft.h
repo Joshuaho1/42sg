8095f3363c4fdecdbf381c3f1dee3c0a0a7c58e5
Josh <josh_ho@rocketmail.com>
2025-06-06 17:07:46 +0800

    added includes and sources for C09

---
+++ piscine/exercises/C09/ex01/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 20:34:19 by joho              #+#    #+#             */
+/*   Updated: 2025/04/10 20:35:51 by joho             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_swap(int *a, int *b);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+
+#endif
