a15e95070a8022d1360cff22739317a2b7c34f7b
Joshho <josh_@Razerr.>
2025-10-29 00:03:56 +0800

    finished parsing portion but doesn't work

--- core/push_swap/push_swap.c
+++ core/push_swap/push_swap.c
@@ -6,62 +6,75 @@
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 15:21:39 by joho              #+#    #+#             */
-/*   Updated: 2025/10/15 16:47:44 by joho             ###   ########.fr       */
+/*   Updated: 2025/10/16 02:08:25 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	push_swap(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	char	**nums;
-	char	**moves;
-	long	**inums;
+	// char	**moves;
+	long	*inums = NULL;
 	int		status;
 
-	status = 0;
-	nums = parse_nums(argc, **argv);
+	if (argc < 2)
+		return (0);
+	nums = parse_nums(argc, argv);
 	status = check_nums(nums, inums);
 	if (!status)
-		return (ft_printf("Error\n"), 1);
-	build_list();
-	sort();
-	moves = print_mov();
-	return (moves);
+	{
+		ft_printf("Error\n");
+		free_split(nums);
+		return (1);
+	}
+	// build_list();
+	// sort();
+	// moves = print_mov();
+	// return (moves);
+	free(inums);
+	free_split(nums);
+	return (0);
 }
 
 char	**parse_nums(int argc, char **argv)
 {
 	char	**nums;
 	char	*joined;
+	char	*tmp;
 	int		i;
 
-	if (argc < 2)
-		exit(1);
 	i = 1;
 	joined = ft_strdup("");
 	if (!joined)
 		return (NULL);
 	while (i < argc)
-		joined = ft_strjoin(joined, argv[i++]);
+	{
+		tmp = ft_strjoin(joined, argv[i++]);
+		free(joined);
+		joined = ft_strjoin(tmp, argv[i]);
+		free(tmp);
+		i++;
+	}
 	nums = ft_split(joined, ' ');
 	free(joined);
 	return (nums);
 }
 
-int	check_nums(char **nums, long **inums)
+int	check_nums(char **nums, long *inums)
 {
 	int		count;
 	long	val;
 	int		i;
 
 	count = 0;
-	i = 0;
 	while (nums[count])
 		count++;
-	*inums = ft_calloc(count, sizeof(long));
-	if (!*inums)
+	inums = ft_calloc(count, sizeof(long));
+	if (!inums)
 		return (0);
+	i = 0;
 	while (nums[i])
 	{
 		if (!valid_digit(nums[i]))
@@ -69,9 +82,10 @@ int	check_nums(char **nums, long **inums)
 		val = ft_atoi(nums[i]);
 		if (val < INT_MIN || val > INT_MAX)
 			return (0);
-		(*inums)[i] = val; 
+		inums[i] = val;
+		i++; 
 	}
-	if (has_dupes(*inums, count))
+	if (has_dupes(inums, count))
 		return (0);
 	return (1);
 }
@@ -94,7 +108,7 @@ int	valid_digit(char *s)
 	return (1);
 }
 
-int	has_dupes(int *arr, int n)
+int	has_dupes(long *arr, int n)
 {
 	int	i;
 	int	j;
@@ -112,4 +126,17 @@ int	has_dupes(int *arr, int n)
 		i++;
 	}
 	return (0);
+}
+
+void	free_split(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr && arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
 }
--- core/push_swap/push_swap.h
+++ core/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 15:27:26 by joho              #+#    #+#             */
-/*   Updated: 2025/10/15 16:33:38 by joho             ###   ########.fr       */
+/*   Updated: 2025/10/16 02:08:10 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 #include <limits.h>
 #include "libft.h"
 
-int		push_swap(int argc, char **argv);
+int		main(int argc, char **argv);
 char	**parse_nums(int argc, char **argv);
+int		check_nums(char **nums, long *inums);
+int		valid_digit(char *s);
+int		has_dupes(long *arr, int n);
+void	free_split(char **arr);
 
 #endif
