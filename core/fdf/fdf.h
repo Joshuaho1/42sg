2ea69d3cfc43da4a214f7cc211fa6095598ebe96
Joshho <josh_@Razerr.>
2025-11-16 19:21:41 +0800

    building reading of map

--- core/fdf/fdf.h
+++ core/fdf/fdf.h
@@ -6,17 +6,21 @@
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 23:31:49 by joho              #+#    #+#             */
-/*   Updated: 2025/11/16 00:42:01 by joho             ###   ########.fr       */
+/*   Updated: 2025/11/16 18:50:15 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef FDF_H
+#ifndef FDF_H
 # define FDF_H
 
 # include "libft/libft.h"
 # include "minilibx-linux/mlx.h"
 
+typedef struct s_map
+{
+	int	**matrix;
+	int	width;
+	int	height;
+}	t_map;
 
-
-# endif
-
+#endif
--- core/fdf/read_map.c
+++ core/fdf/read_map.c
@@ -1,24 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   read_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/15 23:31:46 by joho              #+#    #+#             */
-/*   Updated: 2025/11/16 18:50:40 by joho             ###   ########.fr       */
+/*   Created: 2025/11/16 17:37:29 by joho              #+#    #+#             */
+/*   Updated: 2025/11/16 19:21:01 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(int ac, char **av)
+void	read_map(char *av)
 {
-	int	fd;
+	int		fd;
+	t_map	*map;
+	char	*data;
+	char	**split_d;
 
-	if (ac != 2)
-		return (1);
-	read_map();
-	build_matrix();
-	return (0);
+	if (!av)
+		return ;
+	fd = open(map, O_RDONLY);
+	map->height = 0;
+	data = get_next_line(fd);
+	split_d = ft_split(data, ' ');
+	map->width = 0;
+	while (split_d[map->width] != NULL)
+		(map->width)++;
+	while (data = get_next_line(fd) != NULL)
+		(map->height)++;
+	map->matrix = malloc(map->height * sizeof (int *));
+	
+	close(fd);
 }
