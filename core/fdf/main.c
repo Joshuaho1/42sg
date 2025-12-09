226b68bc395c96d2a57f5e32a4a677581ed38f5e
Josh <josh_ho@rocketmail.com>
2025-12-09 17:20:20 +0800

    fixed non existent file and no permission

--- core/fdf/main.c
+++ core/fdf/main.c
@@ -6,18 +6,25 @@
 /*   By: joho <joho@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 23:31:46 by joho              #+#    #+#             */
-/*   Updated: 2025/12/08 23:44:20 by joho             ###   ########.fr       */
+/*   Updated: 2025/12/09 17:14:33 by joho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void	mlx_live(t_mlx	*mlx);
+
 int	main(int ac, char **av)
 {
 	t_mlx	mlx;
+	int		fd;
 
 	if (ac != 2)
 		return (1);
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		return (perror("Error"), 1);
+	close(fd);
 	mlx.map = read_map(av[1]);
 	if (!mlx.map)
 		return (free_map(mlx.map), 1);
@@ -31,8 +38,13 @@ int	main(int ac, char **av)
 		return (destroy_free(&mlx), 1);
 	init_image(&mlx);
 	render(&mlx);
-	mlx_key_hook(mlx.mlx_wind, handle_input, &mlx);
-	mlx_hook(mlx.mlx_wind, 17, 0, close_window, &mlx);
-	mlx_loop(mlx.mlx_ptr);
+	mlx_live(&mlx);
 	return (0);
+}
+
+static void	mlx_live(t_mlx	*mlx)
+{
+	mlx_key_hook(mlx->mlx_wind, handle_input, mlx);
+	mlx_hook(mlx->mlx_wind, 17, 0, close_window, mlx);
+	mlx_loop(mlx->mlx_ptr);
 }
